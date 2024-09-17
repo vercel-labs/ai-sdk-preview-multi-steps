df7f2cb903d78dd773e680829a39f3adae1c6417
Nico Albanese <nico.albanese@vercel.com>
2024-09-17 18:37:43 +0100

    add analytics

--- app/(preview)/layout.tsx
+++ app/(preview)/layout.tsx
@@ -1,12 +1,12 @@
 import "./globals.css";
 import { Metadata } from "next";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://ai-sdk-preview-roundtrips.vercel.app"),
+  metadataBase: new URL("https://ai-sdk-preview-steps-reasoning.vercel.app"),
   title: "Multi-Step Reasoning with the AI SDK",
-  description:
-    "Reasoning with multi-step generations and the AI SDK",
+  description: "Reasoning with multi-step generations and the AI SDK",
 };
 
 export default function RootLayout({
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Analytics />
         <Toaster position="top-center" richColors />
         {children}
       </body>
